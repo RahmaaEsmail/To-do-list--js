0f993281c0416b1ac1767b35f0dac5034e84fad6
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-05-17 02:18:15 +0300

    update all js

--- js/script.js
+++ js/script.js
@@ -1,377 +1,253 @@
-const addNoteBtn = document.querySelectorAll('.add-note');
+const addBtn = document.querySelectorAll('.add-note');
 const startedBtn = document.querySelector('.add-btn');
 const progressBtn = document.querySelector('.add-progress-btn');
 const completedBtn = document.querySelector('.add-completed-btn');
-let drag = null;
 
-let dataListStarted = [];
-let dataListProgress = [];
-let dataListCompleted = [];
-
-addNoteBtn.forEach(addBtn => {
-    addBtn.addEventListener('click', (e) => {
-        const closeInput = e.target.closest('div');
-        if (closeInput.classList.contains('not-started')) {
-            let btn = closeInput.querySelector('.add-note')
-            displayData(btn)
-            getUserData(closeInput, 'not-started', dataListStarted)
-
-        }
-
-        if (closeInput.classList.contains('in-progress')) {
-            let btn = closeInput.querySelector('.add-note')
-            displayData(btn)
-            getUserData(closeInput, 'in-progress', dataListProgress)
-        }
-
-        if (closeInput.classList.contains('completed')) {
-            let btn = closeInput.querySelector('.add-note')
-            displayData(btn)
-            getUserData(closeInput, 'completed', dataListCompleted)
-        }
-    })
-})
-
-// Display Data on screen
-const displayData = function (btn, data = '') {
-    let li =
-        `
-   <li class="task" draggable="true" ><input type="text" value="${data}"><span><ion-icon name="pencil-outline" class="edit-note"></ion-icon> <ion-icon name="trash-outline" class="delete-note"></ion-icon></span></li>
-   `
-    btn.insertAdjacentHTML('beforebegin', li)
-    // // draggablee()
-    // draggableMouse()
-    // draggableTouch()
-}
+let dataList = [];
 
 
-// Show Existing Data in localstorage
-function updateStartedUi() {
-    if (localStorage.getItem('not-started') != null) {
-        dataListStarted = [...new Set(JSON.parse(localStorage.getItem('not-started')))]
-        dataListStarted = [...new Set(dataListStarted)]
-    }
+const displayStoredData = function () {
+    if (localStorage.getItem('dataList') != null) {
+        dataList = JSON.parse(localStorage.getItem('dataList'));
 
-    dataListStarted.forEach(data => {
-        displayData(startedBtn, data)
-    })
-}
-
-function updateProgressUi() {
-    if (localStorage.getItem('in-progress') != null) {
-        dataListProgress = [...new Set(JSON.parse(localStorage.getItem('in-progress')))]
-        dataListProgress = [...new Set(dataListProgress)]
-    }
-
-    dataListProgress.forEach(data => {
-        displayData(progressBtn, data)
-    })
-}
-
-function updateCompletedUi() {
-    if (localStorage.getItem('completed') != null) {
-        dataListCompleted = [...new Set(JSON.parse(localStorage.getItem('completed')))]
-        dataListCompleted = [...new Set(dataListCompleted)]
-    }
-
-    dataListCompleted.forEach(data => {
-        displayData(completedBtn, data)
-    })
-}
-
-const updateUi = function () {
-    if (updateCompletedUi() || updateProgressUi() || updateStartedUi()) {
-        updateCompletedUi()
-        updateProgressUi()
-        updateStartedUi()
-    }
-
-    const inputs = document.querySelectorAll('input')
-    inputs.forEach(input => {
-        input.setAttribute('disabled', 'true')
-    })
-}
-updateUi()
-
-
-
-// Get Value from input
-function getUserData(closeInput, inputName, dataListName) {
-    let inputs = closeInput.querySelectorAll('input')
-    inputs.forEach(input => {
-        input.addEventListener('change', (e) => {
-
-            dataListName.push(input.value)
-            localStorage.setItem(inputName, JSON.stringify([...new Set(dataListName)]))
-            input.setAttribute('disabled', 'true')
-        })
-    })
-}
-
-// Delete Task
-function deleteProcess(e, listType, storageName) {
-    const li = e.target.closest('li');
-    const input = li.querySelector('input').value;
-    const index = listType.indexOf(input)
-    li.remove()
-    listType.splice(index, 1)
-    localStorage.setItem(storageName, JSON.stringify(listType))
-}
-
-const deleteTasks = function () {
-    let notes = document.querySelectorAll('.delete-note')
-    notes.forEach(note => {
-        note.addEventListener('click', function (e) {
-            let div = e.target.closest('div')
-            if (div.classList.contains('not-started'))
-                deleteProcess(e, dataListStarted, 'not-started')
-
-            if (div.classList.contains('in-progress'))
-                deleteProcess(e, dataListProgress, 'in-progress')
-
-            if (div.classList.contains('completed'))
-                deleteProcess(e, dataListCompleted, 'completed')
-        })
-    })
-}
-deleteTasks()
-
-
-
-// Drag & Drop Mouse Element
-function draggableMouse() {
-    let li = document.querySelectorAll('li');
-    let boxes = document.querySelectorAll('.to-do-list-container div')
-    let closeDiv;
-
-    // Element that dragged
-    li.forEach(item => {
-        item.addEventListener('dragstart', function (e) {
-            closeDiv = e.target.closest('div');
-            drag = item;
-            item.style.opacity = 0.5;
-            e.dataTransfer.effectAllowed = 'move';
-        })
-
-        item.addEventListener('dragend', function (e) {
-            if(e.dataTransfer.dropEffect != 'none') {
-                let input = drag.querySelector('input').value;
-                if(closeDiv.className =='not-started') 
-                    removedDraggedEle(dataListStarted, input, 'not-started')
-                
-
-                if (closeDiv.className == 'in-progress') 
-                    removedDraggedEle(dataListProgress, input, 'in-progress')
-                
-
-                if (closeDiv.className == 'completed') 
-                    removedDraggedEle(dataListCompleted, input, 'completed')
-
+        dataList.forEach(item => {
+            const { list, data, id } = item;
+            if (data && data !== '') {
+                const btn = document.querySelector(`.${list}`).querySelector('.add-note')
+                addDataToBoxes(btn, data)
             }
-            drag = null;
-            item.style.opacity = 1;
+        })
+    }
+}
+
+
+const disableInput = function () {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.disabled = true;
+    })
+}
+
+displayStoredData()
+disableInput()
+
+function addDataToBoxes(btn, dataValue = '') {
+    const li = document.createElement('li');
+    li.innerHTML = `<input type="text" value="${dataValue}"><span><ion-icon name="pencil-outline" class="edit-note"></ion-icon> <ion-icon name="trash-outline" class="delete-note"></ion-icon></span>`
+    li.draggable = true;
+    li.className = 'task';
+    btn.insertAdjacentElement('beforebegin', li)
+
+    deleteTasks(li)
+    editTasks(li)
+    dragTouch(li)
+    dragMouse(li)
+}
+
+const displayData = function (e) {
+    addDataToBoxes(e.target)
+    const inputs = document.querySelectorAll('input');
+    getUserData(inputs)
+}
+
+const getUserData = function (inputs) {
+    inputs.forEach(input => {
+        input.addEventListener('change', storeData)
+    })
+}
+
+const storeData = function (e, data) {
+    let closestDiv = e.target.closest('div').className;
+    let id = Math.floor(e.timeStamp);
+
+    dataList.push(
+        {
+            list: closestDiv,
+            data: data || e.target.value,
+            id: id
+        });
+    removeDuplicatesObjFromArray(dataList)
+    e.target.disabled = true;
+}
+
+function removeDuplicatesObjFromArray(dataList) {
+    const map = new Map();
+    for (let data of dataList) {
+        map.set(data['id'], data)
+    }
+    const iteratorValues = map.values()
+    const uniqueData = [...iteratorValues]
+    localStorage.setItem('dataList', JSON.stringify(uniqueData));
+}
+
+function deleteTasks(...li) {
+    li.forEach(task => {
+        task.addEventListener('click', (e) => {
+            if (e.target.classList.contains('delete-note')) {
+                dataList.forEach(data => {
+                    if (data['data'] && data['id']) {
+                        const index = dataList.indexOf(data)
+                        dataList.splice(index, 1)
+                        localStorage.setItem('dataList', JSON.stringify(dataList))
+                        task.remove()
+                    }
+                })
+            }
         })
     })
+}
 
-    // box in which drop happend
+function editTasks(...li) {
+    li.forEach(item => {
+        item.addEventListener('click', function (e) {
+            if (e.target.classList.contains('edit-note')) {
+                const input = e.target.closest('li').querySelector('input')
+                input.disabled = false;
+                input.focus()
+
+                const div = item.closest('div').className;
+
+                dataList.forEach(data => {
+                    if (data['list'] === div && data['data'] == input.value) {
+
+                        input.addEventListener('change', function (e) {
+                            data['data'] = input.value;
+                            localStorage.setItem('dataList', JSON.stringify(dataList))
+                            disableInput()
+                        })
+
+                    }
+                })
+            }
+        })
+    })
+}
+
+
+
+function dragMouse(...tasks) {
+    const boxes = document.querySelectorAll('.to-do-list-container div');
+    let closestStartDiv;
+    let drag = null;
+
+    tasks.forEach(task => {
+        task.addEventListener('dragstart', dragMouseStart)
+    })
+
+    tasks.forEach(task => {
+        task.addEventListener('dragend', dragMouseEnd)
+    })
+
     boxes.forEach(box => {
-        box.addEventListener('dragover', (e) => {
-                e.preventDefault()
-        })
+        box.addEventListener('dragover', dragMouseOver)
+    })
 
-        box.addEventListener('drop', (e) => {
-            e.preventDefault()
-            box.querySelector('.add-note').insertAdjacentElement('beforebegin', drag)
-
-            if (box.className == 'not-started')
-                dropElement(drag, dataListStarted, 'not-started')
-
-
-            if (box.className == 'in-progress')
-                dropElement(drag, dataListProgress, 'in-progress')
-
-
-            if (box.className == 'completed')
-                dropElement(drag, dataListCompleted, 'completed')
-
-        })
-
+    boxes.forEach(box => {
+        box.addEventListener('drop', dropMouse.bind(this))
     })
 }
 
-function dropElement(dragEle, listType, storageName) {
-    let input = dragEle.querySelector('input')
-    listType.push(input.value)
-    localStorage.setItem(storageName, JSON.stringify([...new Set(listType)]))
+function dragMouseStart(e) {
+    e.target.style.opacity = 0.5;
+    e.dataTransfer.effectAllowed = 'move';
+    drag = e.target;
+    closestStartDiv = drag.closest('div')
 }
 
-function removedDraggedEle (listType,input,storageName) {
-    let index = listType.indexOf(input);
-    listType.splice(index, 1)
-    localStorage.setItem(storageName, JSON.stringify([...new Set(listType)]))
+function dragMouseEnd(e) {
+    const className = closestStartDiv.className;
+    const input = drag.querySelector('input').value;
+    e.target.style.opacity = 1;
+
+    dataList.forEach(data => {
+        if (data['list'] === className && data['data'] === input) {
+            const index = dataList.indexOf(data);
+            dataList.splice(index, 1)
+            removeDuplicatesObjFromArray(dataList)
+        }
+    })
+    drag = null;
+}
+
+function dragMouseOver(e) {
+    e.preventDefault()
+}
+
+function dropMouse(e) {
+    const btn = e.target.closest('div').querySelector('.add-note');
+    const input = drag.querySelector('input').value
+    btn.insertAdjacentElement('beforebegin', drag)
+    storeData(e, input)
 }
 
 
 
-function draggableTouch() {
-    const li = document.querySelectorAll('.task input');
-    let drag = false
-    let closeLi , closeStartDiv , closeEndDiv;
-    li.forEach(item => {
-        item.addEventListener('touchstart',(e)=>{
-            drag = true;
-            closeLi = e.target.closest('li');
-            closeStartDiv = closeLi.closest('div')
-           
-            closeLi.style.position = 'absolute';
-            closeLi.style.opacity = 0.5;
-        })
+function dragTouch(...tasks) {
+    let closestStartDiv, closestEndDiv, drag;
+    tasks.forEach(task => {
+        task.addEventListener('touchstart', dragTouchStart)
     })
 
+    tasks.forEach(task => {
+        task.addEventListener('touchend', dragTouchEnd)
+    })
 
-    li.forEach(item => {
-        item.addEventListener('touchend', (e) => {
-            closeLi = e.target.closest('li');
-            closeEndDiv = closeLi.closest('div')
-            closeLi.style.position = 'relative';
-            closeLi.style.top = 'auto';
-            closeLi.style.left = 'auto';
-            closeLi.style.width = 'auto'
-            closeLi.style.opacity = 1;
-            drag = false;
+    tasks.forEach(task => {
+        task.addEventListener('touchmove', dragTouchMove)
+    })
+}
 
-        if(!drag) {
-                if(closeStartDiv.className === 'not-started') {
-                    dataListStarted =  [...new Set(dataListStarted)]
-                    const index = dataListStarted.indexOf(item.value);
-                    dataListStarted.splice(index,1)
+function dragTouchStart(e) {
+    e.stopPropagation()
+    closestStartDiv = e.target.closest('div');
+    e.target.closest('li').classList.add('drop_touch--start')
+    drag = e.target.closest('li').querySelector('input');
+}
 
-                    localStorage.setItem('not-started', JSON.stringify([...new Set(dataListStarted)]))
-                }
+function dragTouchEnd(e) {
+    e.stopPropagation()
+    closestEndDiv = e.target.closest('div');
+    e.target.closest('li').classList.add('drop_touch--end')
 
-
-                if (closeStartDiv.className === 'in-progress') {
-                    const index = [... new Set(dataListProgress)].indexOf(item.value);
-                    dataListProgress = [...new Set(dataListProgress)]
-                    console.log(dataListProgress);
-                    dataListProgress.splice(index, 1)
-                    console.log(dataListProgress);
-                    localStorage.setItem('in-progress', JSON.stringify([...new Set(dataListProgress)]))
-                }
-
-                if (closeStartDiv.className === 'completed') {
-                    drag = false;
-                    dataListCompleted = [...new Set(dataListCompleted)]
-                    console.log(dataListCompleted);
-                    const index = dataListCompleted.indexOf(item.value);
-                    console.log(dataListCompleted);
-                    dataListCompleted.splice(index,1)
-                    console.log(dataListCompleted);
-                    localStorage.setItem('completed', JSON.stringify([...new Set(dataListCompleted)]))
-                }
+    if (closestStartDiv !== closestEndDiv) {
+        dataList.forEach(data => {
+            if (data['list'] === closestStartDiv.className && data['data'] === drag.value) {
+                const index = dataList.indexOf(data);
+                dataList.splice(index, 1)
+                localStorage.setItem('dataList', JSON.stringify(dataList))
+                removeDuplicatesObjFromArray(dataList)
             }
         })
-    })
-    
 
-    li.forEach(item => {
-        item.addEventListener('touchmove', (e) => {
-            e.preventDefault()
-            if(closeLi.getAttribute('draggable')) {
-            closeLi = e.target.closest('li');
-            closeLi.style.top = `${e.target.offsetHeight}px`;
-            closeLi.style.width = '300px';
-
-            
-            const ourEle = document.elementsFromPoint(
-                e.touches[0].clientX,
-                e.touches[0].clientY,
-            )
-
-            ourEle.forEach(ele => {
-               
-                if(ele.className == 'in-progress' ) {
-                    const taskBtn = ele.querySelector('.add-note');
-                    taskBtn.insertAdjacentElement('beforebegin', closeLi)
-                    dataListProgress.push(item.value)
-                    localStorage.setItem('in-progress', JSON.stringify([...new Set(dataListProgress)]))
-                    
-                }
-
-                if (ele.className == 'not-started') {
-                    const taskBtn = ele.querySelector('.add-note');
-                    taskBtn.insertAdjacentElement('beforebegin', closeLi)
-                    dataListStarted.push(item.value)
-                    localStorage.setItem('not-started', JSON.stringify([...new Set(dataListStarted)]))
-
-                }
-
-                if (ele.className == 'completed') {
-                    const taskBtn = ele.querySelector('.add-note');
-                    taskBtn.insertAdjacentElement('beforebegin', closeLi)
-                    dataListCompleted.push(item.value)
-                    localStorage.setItem('completed', JSON.stringify([...new Set(dataListCompleted)]))
-
-                }
-            })}
-        })
-    
-    })
-   
-
-
-}
-
-
-// Edit Tasks
-function editProcess(storageName, listType, input) {
-
-    const indexOfEditData = listType.indexOf(input.value);
-    if (indexOfEditData > -1) {
-        input.addEventListener('change', function (e) {
-            listType[indexOfEditData] = e.target.value;
-            localStorage.setItem(storageName, JSON.stringify(listType))
-        })
+        storeData(e, drag.value)
     }
-
 }
 
-function editTasks() {
-    const editIcon = document.querySelectorAll('.edit-note');
-    editIcon.forEach(edit => {
-        edit.addEventListener('click', function (e) {
-            let divClass = e.target.closest('div').className;
-            let closeInput = e.target.closest('li').querySelector('input');
-            closeInput.removeAttribute('disabled')
-            closeInput.focus()
+function dragTouchMove(e) {
+    e.preventDefault()
+    if (e.target.closest('li').getAttribute('draggable')) {
+        const li = e.target.closest('li');
 
+        li.style.top = `${e.target.clientY}px`;
 
-            if (divClass === 'not-started') {
-                editProcess('not-started', dataListStarted, closeInput)
-            }
+        const ourEle = document.elementsFromPoint(
+            e.touches[0].clientX,
+            e.touches[0].clientY,
+        )
 
-            if (divClass === 'in-progress') {
-                editProcess('in-progress', dataListProgress, closeInput)
-            }
-
-            if (divClass === 'completed') {
-                editProcess('completed', dataListCompleted, closeInput)
+        ourEle.forEach(ele => {
+            if (ele.className === 'not-started' || ele.className === 'in-progress' || ele.className === 'completed') {
+                let btn = ele.querySelector('.add-note');
+                btn.insertAdjacentElement('beforebegin', li)
             }
         })
-    })
+
+    }
 }
 
-editTasks()
 
-const details = navigator.userAgent;
-let regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-let isMobileDevice = regexp.test(details);
 
-if (isMobileDevice) {
-  draggableTouch()
-} else {
-  draggableMouse()
-}
-
-draggableMouse()
-draggableTouch()
+// Event Listeners
+addBtn.forEach(add => {
+    add.addEventListener('click', displayData);
+})
